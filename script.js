906589399de09e74688eaad04aa029a274677595
Z-67 <121440746+Z-67@users.noreply.github.com>
2023-07-16 23:22:54 +0100

    finished

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ const roundDiv = document.querySelector('.round')
 const winDiv = document.querySelector('.win-statement')
 const loseDiv = document.querySelector('.lose-statement')
 const tieDiv = document.querySelector('.tie-statement')
+const gameoverDiv = document.querySelector('.game-over')
+const gamewinnerDiv = document.querySelector('.game-winner')
+const gameloserDiv = document.querySelector('.game-loser')
+
+let playerScore = 0;
+let computerScore = 0;
+let round= 0;
 
 function getComputerChoice() {
     // Generate a random number between 0 and 2
@@ -24,25 +31,6 @@ function getComputerChoice() {
     }
 
 }
-
-let playerScore = 0;
-let computerScore = 0;
-let round= 0;
-
-function game() {
-
-  rockButton.addEventListener('click', function() {
-    playRound('rock', getComputerChoice());
-  });
-      
-  paperButton.addEventListener('click', function() {
-    playRound('paper', getComputerChoice());
-  });
-      
-  scissorButton.addEventListener('click', function() {
-    playRound('scissors', getComputerChoice());
-  });
-
 
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase(); // Convert playerSelection to lowercase
@@ -79,21 +67,50 @@ function playRound(playerSelection, computerSelection) {
     }
 
 
-
-  if (round <=5) {
-      // End the game
-    console.log("Game Over!");
-    if (playerScore > computerScore) {
-      console.log("Congratulations! You win!");
-    } else if (playerScore < computerScore) {
-      console.log("Oops! You lose!");
-    } else {
-      console.log("It's a tie!");
-      }
-    }
+function checkScore() {
+  if (playerScore === 5 || computerScore === 5) {
+    endGame();
   }
-  
+}
+    
+function endGame() {
+      // Display final result based on the scores
+  gameoverDiv.textContent='Game over!';
+  if (playerScore > computerScore) {
+    gamewinnerDiv.textContent= 'WOW you have won the game!'
+  } else if (playerScore < computerScore) {
+      gameloserDiv.textContent = 'oops you have lost the game :('
+    } 
+    
+      // Remove the event listeners to stop the game
+      rockButton.removeEventListener('click', game);
+      paperButton.removeEventListener('click', game);
+      scissorButton.removeEventListener('click', game);
+    }
+
+function game() {
+
+  rockButton.addEventListener('click', function() {
+    if (playerScore < 5 && computerScore < 5) {
+      playRound('rock', getComputerChoice());
+      checkScore();
+    }
+  });
+
+  paperButton.addEventListener('click', function() {
+    if (playerScore < 5 && computerScore < 5) {
+      playRound('paper', getComputerChoice());
+      checkScore();
+    }
+  });
+
+  scissorButton.addEventListener('click', function() {
+    if (playerScore < 5 && computerScore < 5) {
+      playRound('scissors', getComputerChoice());
+      checkScore();
+    }
+  });
+}
+
+
 game();
-
-
-
